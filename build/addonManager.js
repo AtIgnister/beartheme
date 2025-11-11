cd99327710283f06872988c97e99c450461f6dc7
Tim <137562253+AtIgnister@users.noreply.github.com>
2025-11-11 21:25:52 +0100

    start working on the parser

--- build/addonManager.js
+++ build/addonManager.js
@@ -15,3 +15,10 @@ BEARTHEME_META_ADDON_END;
 
   customStyles.innerHTML += addonCSS
 }
+
+export function parseAddonCssBlock(css) {
+  // I know, I know, use a regex to solve a problem, now you have two problems
+  // shuddup, im too lazy to write a real parser
+  const regex =/^BEARTHEME_META_ADDON_START;((.|\n)*)^BEARTHEME_META_ADDON_END/;
+  let result = regex.exec(css)
+}
